991b27c390d799d3d8daf3f2fac91438957a09ba
rachels-archive <rachelhui@protonmail.com>
2024-03-13 11:06:15 +0800

    ✨New: Create new types and services to get books from API

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BooksService {
+  constructor(private apiService: ApiService) {}
 
-  constructor() { }
+  getBooks(url: string, params: any): Observable<any> {
+    return this.apiService.get(url, params);
+  }
 }
