048c2207e40521535c3e381516d41ff12bf82585
rachels-archive <rachelhui@protonmail.com>
2024-03-19 09:30:42 +0800

    ✨New: Overload CRUD methods in Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,24 +18,59 @@ export class HomeComponent implements OnInit {
 
   bookCount: number = 0;
 
+  API_ENDPOINT = 'http://localhost:3000/books';
+
   onSelectBook(book: Book) {
     console.log(book);
   }
 
-  fetchBooks(page: number, perPage: number) {
+  getBooks(page: number, perPage: number) {
     this.booksService
-      .getBooks('http://localhost:3000/books', { page: page, perPage: perPage })
+      .getBooks(`${this.API_ENDPOINT}`, { page: page, perPage: perPage })
       .subscribe((books: Books) => {
         this.books = books.items;
         this.bookCount = books.total;
       });
   }
 
+  editBook(book: Book, id: number) {
+    this.booksService.editBook(`${this.API_ENDPOINT}/${id}`, book).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  deleteBook(id: number) {
+    this.booksService.deleteBook(`${this.API_ENDPOINT}/${id}`).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  addBook(book: Book) {
+    this.booksService.addBook(`${this.API_ENDPOINT}`, book).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
   onPageChange(event: PaginatorState) {
-    this.fetchBooks(event?.page || 0, event?.rows || 8);
+    this.getBooks(event?.page || 0, event?.rows || 8);
   }
 
   ngOnInit(): void {
-    this.fetchBooks(0, 8);
+    this.getBooks(0, 8);
   }
 }
